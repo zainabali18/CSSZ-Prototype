10a2f35e5e91996a8228d70e85a016ee5ef3c1f4
Michael B. Gale <github@michael-gale.co.uk>
2024-11-17 14:54:54 +0000

    Add `apply` script

---
+++ src/apply.ts
@@ -0,0 +1,24 @@
+import { writeFileSync } from "fs";
+import { GROUP_CONFIG_FILE, PROTOTYPE_GROUPS_CATEGORY } from "./const";
+import { createGroup, readGroups } from "./groups";
+import { synchronise } from "./synchronise";
+
+async function runWrapper() {
+  const results = await synchronise();
+  const events = results.events;
+
+  try {
+    for (let index = 0; index < events.groupsToCreate.length; index++) {
+      const event = events.groupsToCreate[index];
+      const group = await createGroup(PROTOTYPE_GROUPS_CATEGORY, event.name);
+      event.specification.id = group.id;
+    }
+
+    writeFileSync(GROUP_CONFIG_FILE, JSON.stringify(results.configGroups));
+  } catch (err) {
+    console.error(`Failed to read actions from file: ${err}`);
+    process.exit(1);
+  }
+}
+
+runWrapper();
