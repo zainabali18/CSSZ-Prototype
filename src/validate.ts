2a02731431eca4f84e623b57bbb68391d7b2423c
Michael B. Gale <github@michael-gale.co.uk>
2024-11-17 13:57:24 +0000

    Only write markdown file if there are changes

--- src/validate.ts
+++ src/validate.ts
@@ -135,7 +135,15 @@ async function runWrapper() {
 
   configGroups.forEach(validateExistingGroup);
 
-  writeMarkdown(events);
+  // Write a summary of the changes as markdown, if there are any changes.
+  if (
+    events.groupsToCreate.length > 0 ||
+    events.groupsToUpdate.length > 0 ||
+    events.membersToAdd.length > 0 ||
+    events.membersToRemove.length > 0
+  ) {
+    writeMarkdown(events);
+  }
 }
 
 runWrapper();
