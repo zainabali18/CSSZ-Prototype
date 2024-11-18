ff7cfcf4c1606c486d7e43b84dccbf08a13f37d3
Michael B. Gale <github@michael-gale.co.uk>
2024-11-18 01:59:19 +0000

    Accept explicit verb in `makeCanvasRequest`

--- src/canvas.ts
+++ src/canvas.ts
@@ -6,14 +6,16 @@ import { CANVAS_BASE_URL } from "./const";
  *
  * @param url The API endpoint.
  * @param body Optionally, data to send as the request body.
+ * @param verb Optionally, the HTTP verb to use.
  * @returns Returns the initialised request.
  */
 export function makeCanvasRequest(
   url: string,
   body?: BodyInit | undefined,
+  verb?: "GET" | "POST" | "DELETE" | "PUT",
 ): Request {
   const request = new Request(`${CANVAS_BASE_URL}/${url}`, {
-    method: body === undefined ? "GET" : "POST",
+    method: verb === undefined ? (body === undefined ? "GET" : "POST") : verb,
     body,
   });
   request.headers.append("Authorization", `Bearer ${process.env.CANVAS_TOKEN}`);
