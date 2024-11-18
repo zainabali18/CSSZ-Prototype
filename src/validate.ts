7e51161ae07dac737dff202b5ea857e9d68974ca
Michael B. Gale <github@michael-gale.co.uk>
2024-11-18 18:09:10 +0000

    Catch exceptions in `validate.ts` and exit with a non-zero status code

--- src/validate.ts
+++ src/validate.ts
@@ -1,9 +1,18 @@
-import { writeMarkdown } from "./event";
+import { Events, writeMarkdown } from "./event";
 import { synchronise } from "./synchronise";
 
+async function runValidate(): Promise<Events> {
+  try {
+    const results = await synchronise();
+    return results.events;
+  } catch (err) {
+    console.error(`Failed to validate configuration: ${err}`);
+    process.exit(1);
+  }
+}
+
 async function runWrapper() {
-  const results = await synchronise();
-  const events = results.events;
+  const events = await runValidate();
 
   // Write a summary of the changes as markdown, if there are any changes.
   if (
